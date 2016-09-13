e9b537ccd2a16ae0005233b948b8d4c76649cc6c
YoioReloaded <yoioreloaded@gmail.com>
2016-09-13 17:46:08 +0200

    Add database support

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-var express = require('express');
-var app = express();
+var pg = require('pg')
+var express = require('express')
 
-app.set('port', (process.env.PORT || 5000));
+var app = express()
+app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'))
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.send('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Seriously, what am I doing!?</title><link href="/stylesheets/default.css" rel="stylesheet" /></head><body><img src="/media/img/nodejs.png" /><p>Node. Just... Node, ok?</p></body></html>');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/db/info', function (request, response) {
+  if ('true' === process.env.DEBUG) {
+    response.send(undefined == process.env.DATABASE_URL ? "Not a single DB URL was given that day..." : process.env.DATABASE_URL)
+  } else {
+    response.send('I got nothing to say...')
+  }
+})
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
 });
 
 
